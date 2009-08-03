32d4a7e18611838341a14bd9b0862ba53ce45365
Chrissy Wainwright <chrissy@sixfeetup.com>
2009-08-03 21:10:49 +0000

    added a control panel for the slideshow via Site Setup for easy customization functionality

--- collective/easyslideshow/browser/interfaces.py
+++ collective/easyslideshow/browser/interfaces.py
@@ -1,10 +1,70 @@
 from zope.interface import Interface
+from zope import schema
+from zope.i18nmessageid import MessageFactory
+
+_ = MessageFactory('collective.easyslideshow')
 
 
 class IEasySlideshowBrowserLayer(Interface):
     """Browser layer marker interface
     """ 
 
+class IEasyslideshowConfiguration(Interface):
+  """This interface defines the configlet for the slideshows."""
+
+  slideshow_width = schema.Int(title=_(u"label_slideshow_width", default=u'Width of the slideshow'),
+                               description=_(u"help_slideshow_width",
+                                             default=u'Enter a whole number for width in pixels.'),
+                               required=True,
+                               default=500)
+  
+  slideshow_height = schema.Int(title=_(u"label_slideshow_height", default=u'Height of the slideshow'),
+                                description=_(u"help_slideshow_height",
+                                              default=u'Enter a whole number for height in pixels.'),
+                                required=True,
+                                default=333)
+                               
+  slide_timeout = schema.Int(title=_(u"label_slide_timeout", default=u'Slide Time'),
+                             description=_(u"help_slide_timeout",
+                                           default=u"Enter a number in milliseconds. "
+                                           "Entering '0' will set the slideshow to only be "
+                                           "manually operated using the navigation"),
+                             required=True,
+                             default=7000)
+                             
+  transition = schema.Choice(title=_(u"label_transition", default=u'Transition'),
+                             description=_(u"help_transition",
+                                           default=u''),
+                             values=("blindX", "blindY", "blindZ", "cover", "curtainX", "curtainY",
+                             "fade", "fadeZoom", "growX", "growY", "scrollUp", "scrollDown", "scrollLeft",
+                             "scrollRight", "scrollHorz", "scrollVert", "shuffle", "slideX", "slideY",
+                             "turnUp", "turnDown", "turnLeft", "turnRight", "uncover", "wipe", "zoom"),
+                             default="fade")
+                            
+  transition_speed = schema.Int(title=_(u"label_transition_speed", default=u'Transition Time'),
+                                description=_(u"help_transition_speed",
+                                              default=u'Enter a number in milliseconds.'),
+                                required=True,
+                                default=1000)
+
+  pause_hover = schema.Bool(title=_(u"label_pause_hover", default=u'Pause on Hover'),
+                            description=_(u"help_pause_hover",
+                                          default=u"Enable slideshow pausing when cursor "
+                                          "is hovering over the slideshow"),
+                            required=False,
+                            default=False)
+                            
+  display_nav = schema.Bool(title=_(u"label_display_nav", default=u'Display Navigation'),
+                            description=_(u"help_display_nav",
+                                          default=u''),
+                            required=True,
+                            default=True)
+                          
+  display_caption = schema.Bool(title=_(u"label_display_caption", default=u'Display Caption'),
+                                description=_(u"help_display_caption",
+                                              default=u''),
+                                required=True,
+                                default=True)
 
 class IEasySlideshowLiteBrowserLayer(Interface):
     """Browser layer marker interface for the lite version
