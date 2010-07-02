bb59504f8cfc332f10cd3840b2c699681b40edba
Clayton Parker <me@claytron.com>
2010-07-02 15:10:40 +0000

    Updating to a modern test base

--- collective/easyslideshow/tests/test_doctest.py
+++ collective/easyslideshow/tests/test_doctest.py
@@ -1,40 +1,18 @@
 import unittest
-
-from zope.testing import doctestunit
-from zope.component import testing
+import doctest
 from Testing import ZopeTestCase as ztc
-
-from Products.Five import zcml
-from Products.Five import fiveconfigure
-from Products.PloneTestCase import PloneTestCase as ptc
-from Products.PloneTestCase.layer import PloneSite
-ptc.setupPloneSite()
-
-import collective.easyslideshow
+from test.foobar.tests import base
 
 
 def test_suite():
     return unittest.TestSuite([
 
-        # Unit tests
-        #doctestunit.DocFileSuite(
-        #    'README.txt', package='collective.easyslideshow',
-        #    setUp=testing.setUp, tearDown=testing.tearDown),
-
-        #doctestunit.DocTestSuite(
-        #    module='collective.easyslideshow.mymodule',
-        #    setUp=testing.setUp, tearDown=testing.tearDown),
-
-
-        # Integration tests that use PloneTestCase
-        #ztc.ZopeDocFileSuite(
-        #    'README.txt', package='collective.easyslideshow',
-        #    test_class=TestCase),
-
-        #ztc.FunctionalDocFileSuite(
-        #    'browser.txt', package='collective.easyslideshow',
-        #    test_class=TestCase),
-
+        # Demonstrate the main content types
+        ztc.ZopeDocFileSuite(
+            'configlet.txt', package='collective.easyslideshow',
+            test_class=base.FunctionalTestCase,
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE |
+                doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS),
         ])
 
 if __name__ == '__main__':
