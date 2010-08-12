183f346f63b6566691d4a2d0d905d99fbc856f05
tbesluau <tbesluau@sixfeetup.com>
2010-08-12 13:15:27 +0000

    making sure there will be no duplicate folder icon fi plone4 is used

--- src/collective/easyslideshow/Extensions/Install.py
+++ src/collective/easyslideshow/Extensions/Install.py
@@ -1,3 +1,4 @@
+import sys
 from cStringIO import StringIO
 from Products.CMFCore.utils import getToolByName
 
@@ -11,6 +12,12 @@ def install(portal):
     """Run the GS profile to install this package"""
     out = StringIO()
     runProfile(portal, 'profile-collective.easyslideshow:default')
+    # if plone 4 is used, we need to remove the folder icon GS imported
+    # to do so we check the python version to see if it is 2.6
+    if sys.version_info[:2] == (2, 6):
+        pt = getToolByName(portal, 'portal_types')
+        pt['Folder'].icon_expr = ''
+        pt['Folder'].icon_expr_object = None
     print >> out, "Installed collective.easyslideshow"
     return out.getvalue()
 
