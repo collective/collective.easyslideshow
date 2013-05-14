4331a3a9a47fc1ae82060b6a620e6adaf1ba73a0
Philip Bauer <bauer@starzel.de>
2013-05-14 16:03:53 +0200

    improve uninstall-story

--- src/collective/easyslideshow/Extensions/Install.py
+++ src/collective/easyslideshow/Extensions/Install.py
@@ -7,8 +7,10 @@ from p4a.subtyper import interfaces as p4ainterfaces
 from collective.easyslideshow import interfaces as essinterfaces
 from zope.annotation.interfaces import IAnnotations
 from zope.component import getMultiAdapter
+from zope.component.interfaces import ComponentLookupError
 from plone.portlets.interfaces import IPortletAssignmentMapping
 from plone.portlets.interfaces import IPortletManager
+import logging
 
 
 def runProfile(portal, profileName):
@@ -36,10 +38,14 @@ def uninstall(portal, reinstall=False):
     """Remove slideshow_folder_view from display list, reset folder display"""
     if reinstall:
         return
+    logger = logging.getLogger("collective.easyslideshow")
+
     pt = portal.portal_types
 
     pc = getToolByName(portal, 'portal_catalog')
     brains = pc.searchResults(portal_type='Folder')
+    
+    # remove annotations and interfaces
     for brain in brains:
         folder = brain.getObject()
         if folder.getProperty("layout") is not None:
@@ -54,16 +60,22 @@ def uninstall(portal, reinstall=False):
         if psD and psD.get('descriptor_name')\
            and psD['descriptor_name'] == 'collective.easyslideshow.slideshow':
             annotations.pop('p4a.subtyper.DescriptorInfo')
+
+    # remove portlet-assignments   
     allbrains = pc()
     for brain in allbrains:
         item = brain.getObject()
         for column in ['plone.leftcolumn', 'plone.rightcolumn']:
             manager = getUtility(IPortletManager, name=column)
-            assignments = getMultiAdapter((item, manager),
+            try:
+                assignments = getMultiAdapter((item, manager),
                                                  IPortletAssignmentMapping)
-            for key in assignments.keys():
-                if key.startswith('slideshow-portlet'):
-                    del assignments[key]
+            except ComponentLookupError, e:
+                logger.error("Ignoring broken portlet: %s" % str(e))
+            if assignments:
+                for key in assignments.keys():
+                    if key.startswith('slideshow-portlet'):
+                        del assignments[key]
 
     avViews = []
     for view in pt['Folder'].view_methods:
@@ -75,5 +87,23 @@ def uninstall(portal, reinstall=False):
     """Run the GS profile to uninstall this package"""
     out = StringIO()
     runProfile(portal, 'profile-collective.easyslideshow:uninstall')
+
+    _removePersistentUtility(portal, logger)
     print >> out, "Uninstalled collective.easyslideshow"
     return out.getvalue()
+
+
+def _removePersistentUtility(portal, logger):
+    sm = portal.getSiteManager()
+    try:
+        from p4a.subtyper.interfaces import IPortalTypedFolderishDescriptor
+        util = sm.getUtility(IPortalTypedFolderishDescriptor, name="collective.easyslideshow.slideshow")
+        sm.unregisterUtility(util)
+        del util
+        sm.utilities.unsubscribe((), IPortalTypedFolderishDescriptor)
+        del sm.utilities.__dict__['_provided'][IPortalTypedFolderishDescriptor]
+        logger.info("Removed utility")
+    except ComponentLookupError, e:
+        logger.error("Error while removing utility: %s" % str(e))
+        logger.info("You might want to try wildcard.fixpersistentutilitie")
+
