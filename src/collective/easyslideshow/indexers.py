219a16702d6aaed81999d4f95eae50d3c481b82b
Chrissy Wainwright <chrissy@sixfeetup.com>
2015-10-30 14:08:22 -0500

    fix related links on slides refs #15

--- src/collective/easyslideshow/indexers.py
+++ src/collective/easyslideshow/indexers.py
@@ -1,28 +1,50 @@
 from Products.CMFCore.utils import getToolByName
+from plone.app.contenttypes.interfaces import IImage
 from plone.indexer import indexer
 from Products.ATContentTypes.interface.image import IATImage
+from Products.CMFPlone.utils import base_hasattr
 from zope.component import provideAdapter
 
+import pkg_resources
 
-@indexer(IATImage)
+try:
+    pkg_resources.get_distribution('plone.app.relationfield')
+except pkg_resources.DistributionNotFound:
+    HAS_RELATIONFIELD = False
+else:
+    from plone.app.relationfield.behavior import IRelatedItems
+    HAS_RELATIONFIELD = True
+
+
+@indexer(IImage)
 def getRelatedLink(obj, **kwargs):
     """Index to get the link for the slide.  It will be the first related
     item.
     """
-    if obj.portal_type == 'Image':
+
+    # Archetypes
+    if base_hasattr(obj, 'getRawRelatedItems'):
         related_items = obj.getRefs('relatesTo')
         if related_items:
             # we're only concerned about the first item
-            image_link = related_items[0]
-            wftool = getToolByName(obj, 'portal_workflow')
-            if image_link:
-                current_state = wftool.getInfoFor(image_link, 'review_state')
-            else:
-                current_state = 'None'
-            # we are assuming the linked to object needs to be published
-            # TODO: make this configurable.
-            if current_state == 'published':
-                return '/'.join(image_link.getPhysicalPath())
-    return
+            return checkPermissions(obj, related_items[0])
+
+    # Dexterity
+    if HAS_RELATIONFIELD and IRelatedItems.providedBy(obj):
+        related_items = obj.relatedItems
+        if related_items:
+            # we're only concerned about the first item
+            return checkPermissions(obj, related_items[0])
+
+    raise AttributeError
+
+
+def checkPermissions(obj, related):
+    """check that the linked item is published
+    """
+    catalog = getToolByName(obj, 'portal_catalog')
+    brain = catalog(path=dict(query=related.to_path, depth=0))
+    return brain[0].getURL()
+
 
 provideAdapter(getRelatedLink, name='getRelatedLink')
